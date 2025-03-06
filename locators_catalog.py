30021fd1c9a89897815f7655f86e0f0a3f06e38f
Nikita <pechka959@gmail.com>
2025-03-06 12:45:02 +0300

    add logger

--- locators_catalog.py
+++ locators_catalog.py
@@ -1,30 +1,33 @@
 from selenium.webdriver.common.by import By
 
+
 class LocatorsCatalog:
+    #CATALOG
+    CATALOG_LOCATOR = (By.CLASS_NAME, "rounded-xl")
 
+    #BRAND
+    BRAND_LOCATOR = (By.ID, "car-brand")
+    BRAND_LIST_LOCATOR = (By.CSS_SELECTOR, "[data-testid='select-option']")
 
-    catalog_loc = (By.CLASS_NAME, "rounded-xl")
-    brand_car_loc = (By.ID, "car-brand")
-    brand_list_loc = (By.CSS_SELECTOR, "[data-testid='select-option']")
-    model_car_loc = (By.ID, "car-model")
-    model_list_loc = (By.CSS_SELECTOR, "[data-testid='rc-select-item-option']")
-    car_location_loc = (By.ID, "car-location")
-    location_list_loc = (By.CSS_SELECTOR, "[data-testid='rc-select-item-option']")
-    car_price_from_loc = (By.ID, "car-price-from")
-    car_price_list_loc = (By.CSS_SELECTOR, "[data-testid='select-option']")
-    car_price_to_loc = (By.ID, "car-price-to")
-    car_price_list_loc = (By.CSS_SELECTOR, "[data-testid='select-option']")
-    car_years_from_loc = (By.ID, "car-years-from")
-    car_years_list_loc = (By.CSS_SELECTOR, "[data-testid='select-option']")
-    car_years_to_loc = (By.ID, "car-years-to")
-    car_years_list_loc = (By.CSS_SELECTOR, "[data-testid='select-option']")
-    owners_loc = (By. CLASS_NAME, "py-3")
-    car_owners_from_loc = (By. ID, "car-owners-from")
-    car_owners_to_loc = (By. ID, "car-owners-to")
-    car_mileage_from_loc = (By. ID, "car-mileage-from")
-    car_mileage_to_loc = (By. ID, "car-mileage-to")
+    #MODEL
+    MODEL_LOCATOR = (By.ID, "car-model")
+    MODEL_LIST_LOCATOR = (By.CSS_SELECTOR, "[data-testid='rc-select-item-option']")
 
+    #LOCATION
+    LOCATION_LOCATOR = (By.ID, "car-location")
+    LOCATION_LIST_LOCATOR = (By.CSS_SELECTOR, "[data-testid='rc-select-item-option']")
 
+    #PRICE
+    PRICE_FROM_LOCATOR = (By.ID, "car-price-from")
+    PRICE_LIST_LOCATOR = (By.CSS_SELECTOR, "[data-testid='select-option']")
+    PRICE_TO_LOCATOR = (By.ID, "car-price-to")
 
-
-
+    #YEARS
+    YEARS_FROM_LOCATOR = (By.ID, "car-years-from")
+    YEARS_LIST_LOCATOR = (By.CSS_SELECTOR, "[data-testid='select-option']")
+    YEARS_TO_LOCATOR = (By.ID, "car-years-to")
+    
+    #OWNERS
+    OWNERS_LOCATOR = (By.CLASS_NAME, "py-3")
+    OWNERS_FROM_LOCATOR = (By.ID, "car-owners-from")
+    OWNERS_TO_LOCATOR = (By.ID, "car-owners-to")
--- test_catalog.py
+++ test_catalog.py
@@ -1,11 +1,13 @@
 from page_catalog import Base
+from page_catalog import BASE_URL
+import pytest
 
-
+@pytest.mark.smoke
 def test_01_page_catalog(browser):
     catalog = Base(browser)
     catalog.open_page()
     catalog.open_catalog_page()
-    catalog.check_catalog_url("https://antcar.ru/catalog")
+    catalog.check_catalog_url(BASE_URL)
     catalog.choose_random_brand_car()
     catalog.choose_random_model_car()
     catalog.choose_random_car_location()
